f81fdb0dc97344eb9271853335547c08cb351c4f
kulbhushan basnet <np03cs4a220500@heraldcollege.edu.np>
2024-12-09 21:52:13 +0545

    Fixing some css

--- internship/src/main.jsx
+++ internship/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/Internship1/'>
     <App/>
     
     </Router>
